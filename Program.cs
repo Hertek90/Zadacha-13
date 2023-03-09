2c84028f278712bc5b65385e3ae7ba5e02966e71
«Apolon» <hertek1990@gmail.com>
2023-03-09 21:52:31 +0700

    Zadacha 13

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-Console.WriteLine("Введите число");
-int i = Convert.ToInt32(Console.ReadLine());
-if (i > 99)
+﻿internal class Program
 {
-   for (i=i; i > 999; i = i / 10);
+    private static void Main(string[] args)
     {
-        System.Console.WriteLine(i % 10);
+        // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        // 645 -> 5
+        // 78 -> третьей цифры нет
+        // 32679 -> 6
+        Console.WriteLine("Введите число");
+        int i = Convert.ToInt32(Console.ReadLine());
+        if (i > 99)
+        {
+            for (i = i; i > 999; i = i / 10) ;
+            {
+                Console.WriteLine(i % 10);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
     }
-}
-else
-{
-    System.Console.WriteLine("Третьей цифры нет");
 }
